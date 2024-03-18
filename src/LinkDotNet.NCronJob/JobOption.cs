7146a0a6153653897d361ee602abf80bde76ed0f
Steven Giesel <stgiesel35@gmail.com>
2024-03-18 17:22:37 +0100

    docs: Explanation for CronExpression extended (InstantJob)

--- src/LinkDotNet.NCronJob/JobOption.cs
+++ src/LinkDotNet.NCronJob/JobOption.cs
@@ -6,8 +6,12 @@ namespace LinkDotNet.NCronJob;
 public sealed class JobOption
 {
     /// <summary>
-    /// Set's the cron expression for the job. If set to null, the job is added to the container but will not be scheduled
+    /// Set's the cron expression for the job. If set to null, the job is added to the container but will not be scheduled.
     /// </summary>
+    /// <remarks>
+    /// The <see cref="IInstantJobRegistry"/> retrieves the job instance from the container, therefore instant jobs have to
+    /// be registered as well. They dont need the <see cref="CronExpression"/> set to something.
+    /// </remarks>
     public string? CronExpression { get; set; }
 
     /// <summary>
