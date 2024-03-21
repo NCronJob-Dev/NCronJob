9417606a5611f12060d27d3c463d8c05255b373f
Steven Giesel <stgiesel35@gmail.com>
2024-03-21 12:44:59 +0100

    refactor: Better test design

--- src/LinkDotNet.NCronJob/TaskExtensions.cs
+++
@@ -1,9 +0,0 @@
-namespace LinkDotNet.NCronJob;
-
-internal static class TaskExtensions
-{
-    public static Task RunInIsolation(this Task t, IsolationLevel isolationLevel)
-    {
-        return isolationLevel == IsolationLevel.None ? t : Task.Run(() => t);
-    }
-}
