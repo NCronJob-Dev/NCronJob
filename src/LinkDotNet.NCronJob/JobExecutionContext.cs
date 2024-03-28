a0d69b50a821a9b03c1afd6b77b7779e6b29192c
Steven Giesel <stgiesel35@gmail.com>
2024-03-28 22:10:27 +0100

    docs: Fixed missing dot

--- src/LinkDotNet.NCronJob/JobExecutionContext.cs
+++ src/LinkDotNet.NCronJob/JobExecutionContext.cs
@@ -3,7 +3,7 @@ namespace LinkDotNet.NCronJob;
 /// <summary>
 /// Represents the context of a job execution.
 /// </summary>
-/// <param name="Parameter">The passed in parameters to a job</param>
+/// <param name="Parameter">The passed in parameters to a job.</param>
 public sealed record JobExecutionContext(object? Parameter)
 {
     /// <summary>
