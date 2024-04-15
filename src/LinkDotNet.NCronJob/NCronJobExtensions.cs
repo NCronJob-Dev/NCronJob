cbfd363988c2d0673c3f5e6a2aa0c99fea95830f
Steven Giesel <stgiesel35@gmail.com>
2024-04-15 21:45:41 +0200

    docs: Added example on AddNCronJob

--- src/LinkDotNet.NCronJob/NCronJobExtensions.cs
+++ src/LinkDotNet.NCronJob/NCronJobExtensions.cs
@@ -13,6 +13,14 @@ public static class NCronJobExtensions
     /// </summary>
     /// <param name="services">The service collection used to register the services.</param>
     /// <param name="options">The builder to register jobs and other settings.</param>
+    /// <example>
+    /// To register a job that runs once every hour with a parameter and a handler that gets notified once the job is completed:
+    /// <code>
+    /// Services.AddNCronJob(options =>
+    ///  .AddJob&lt;MyJob&gt;(c => c.WithCronExpression("0 * * * *").WithParameter("myParameter"))
+    ///  .AddNotificationHandler&lt;MyJobHandler, MyJob&gt;());
+    /// </code>
+    /// </example>
     public static IServiceCollection AddNCronJob(
         this IServiceCollection services,
         Action<NCronJobOptionBuilder>? options = null)
