03ba049c4eb0962ad22b4c94d5c1e739102de1ca
Frank Alvarez <falvarez@wsits.com>
2024-04-29 17:02:37 -0400

    fix reason for test failure with EachJobRunHasItsOwnScope

--- src/LinkDotNet.NCronJob/Registry/CronRegistry.cs
+++ src/LinkDotNet.NCronJob/Registry/CronRegistry.cs
@@ -1,4 +1,5 @@
 using System.Collections.Frozen;
+using System.Collections.Immutable;
 using Microsoft.Extensions.Logging;
 
 namespace LinkDotNet.NCronJob;
@@ -7,7 +8,7 @@ internal sealed partial class CronRegistry : IInstantJobRegistry
 {
     private readonly JobExecutor jobExecutor;
     private readonly ILogger<CronRegistry> logger;
-    private readonly FrozenSet<RegistryEntry> cronJobs;
+    private readonly ImmutableArray<RegistryEntry> cronJobs;
 
     public CronRegistry(IEnumerable<RegistryEntry> jobs,
         JobExecutor jobExecutor,
@@ -15,7 +16,7 @@ internal sealed partial class CronRegistry : IInstantJobRegistry
     {
         this.jobExecutor = jobExecutor;
         this.logger = logger;
-        cronJobs = jobs.Where(c => c.CronExpression is not null).ToFrozenSet();
+        cronJobs = jobs.Where(c => c.CronExpression is not null).ToImmutableArray();
     }
 
     public IReadOnlyCollection<RegistryEntry> GetAllCronJobs() => cronJobs;
