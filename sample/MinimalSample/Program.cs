3a7acb9b9d096843ca672112cbcf14665d5e860d
Frank Alvarez <falvarez@wsits.com>
2024-05-17 20:14:00 -0400

    added more details and examples to the summary comments

--- sample/MinimalSample/Program.cs
+++ sample/MinimalSample/Program.cs
@@ -7,18 +7,4 @@ builder.Services.AddJob((ILogger<Program> logger, TimeProvider timeProvider) =>
     logger.LogInformation("Hello World - The current date and time is {Time}", timeProvider.GetLocalNow());
 }, "*/40 * * * * *");
 
-
-builder.Services.AddJob([RetryPolicy(retryCount: 4)] (JobExecutionContext context, ILogger<Program> logger) =>
-{
-    var attemptCount = context.Attempts;
-
-    if (attemptCount <= 4)
-    {
-        logger.LogWarning("TestRetryJob simulating failure.");
-        throw new InvalidOperationException("Simulated operation failure in TestRetryJob.");
-    }
-
-    logger.LogInformation($"Job ran after {attemptCount} attempts");
-}, "*/5 * * * * *");
-
 await builder.Build().RunAsync();
