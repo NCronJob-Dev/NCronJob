4fc5ae0edcf478114e5ef6e886e031fb8110bf78
Frank Alvarez <falvarez@AdvantaHealth.com>
2024-05-23 18:45:53 -0400

    Enhance NCronJob configuration and service registration
    - Add "GlobalOptions" section with "MaxDegreeOfParallelism" to `appsettings.json`
    - Add FrameworkReference to "Microsoft.AspNetCore.App" in `NCronJob.csproj`
    - Update `NCronJobExtensions.cs` with new `globalOptions` field and methods for global options management and service registration
    - Modify `AddNCronJob` method to use `globalOptions`
    - Add new `AddNCronJob` method for web application builder integration
    - Add private methods for configuring options, registering services, and logging warnings

--- src/NCronJob/NCronJobExtensions.cs
+++ src/NCronJob/NCronJobExtensions.cs
@@ -1,5 +1,8 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
 
 namespace NCronJob;
 
@@ -8,13 +11,20 @@ namespace NCronJob;
 /// </summary>
 public static class NCronJobExtensions
 {
+    private static Lazy<GlobalOptions> globalOptions = new(() => new GlobalOptions());
+
+    /// <summary>
+    /// Used to reset the global options to the default values to use for Unit Testing.
+    /// </summary>
+    internal static void ResetGlobalOptions() => globalOptions = new Lazy<GlobalOptions>(() => new GlobalOptions());
+
     /// <summary>
     /// Adds NCronJob services to the service container.
     /// </summary>
     /// <param name="services">The service collection used to register the services.</param>
     /// <param name="options">The builder to register jobs and other settings.</param>
     /// <param name="configureGlobalOptions">Allows configuration of global behavior across all jobs.
-    /// If multiple instances of <see cref="NCronJobExtensions.AddNCronJob"/> are called with different global settings,
+    /// If multiple instances of "NCronJobExtensions.AddNCronJob" are called with different global settings,
     /// only the first one will be taken into account.</param>
     /// <example>
     /// To register a job that runs once every hour with a parameter and a handler that gets notified once the job is completed:
@@ -29,12 +39,80 @@ public static class NCronJobExtensions
         Action<NCronJobOptionBuilder>? options = null,
         Action<GlobalOptions>? configureGlobalOptions = null)
     {
-        var globalOptions = new GlobalOptions();
-        configureGlobalOptions?.Invoke(globalOptions);
-        var builder = new NCronJobOptionBuilder(services, globalOptions.ToConcurrencySettings());
-        options?.Invoke(builder);
+        ConfigureGlobalOptions(configureGlobalOptions);
 
-        services.TryAddSingleton(globalOptions.ToConcurrencySettings());
+        var builderOptions = new NCronJobOptionBuilder(services, globalOptions.Value.ToConcurrencySettings());
+        options?.Invoke(builderOptions);
+
+        RegisterServices(services);
+
+        return services;
+    }
+
+    /// <summary>
+    /// Adds NCronJob services to the web application builder.
+    /// </summary>
+    /// <param name="builder">The web application builder used to register the services.</param>
+    /// <param name="options">The builder to register jobs and other settings.</param>
+    /// <param name="configureGlobalOptions">Allows configuration of global behavior across all jobs.
+    /// If multiple instances of "AddNCronJob" are called with different global settings,
+    /// only the first one will be taken into account. Subsequent calls will log a warning and use the initial settings.</param>
+    /// <returns>The updated <see cref="WebApplicationBuilder"/>.</returns>
+    /// <example>
+    /// To register a job that runs once every hour with a parameter and a handler that gets notified once the job is completed:
+    /// <code>
+    /// builder.AddNCronJob(options =>
+    ///  .AddJob&lt;MyJob&gt;(c => c.WithCronExpression("0 * * * *").WithParameter("myParameter"))
+    ///  .AddNotificationHandler&lt;MyJobHandler, MyJob&gt;());
+    /// </code>
+    /// </example>
+    public static WebApplicationBuilder AddNCronJob(this WebApplicationBuilder builder,
+        Action<NCronJobOptionBuilder>? options = null,
+        Action<GlobalOptions>? configureGlobalOptions = null)
+    {
+        ArgumentNullException.ThrowIfNull(builder);
+
+        var services = builder.Services;
+        var configuration = builder.Configuration;
+
+        ConfigureGlobalOptions(configureGlobalOptions, configuration);
+
+        var builderOptions = new NCronJobOptionBuilder(services, globalOptions.Value.ToConcurrencySettings());
+        options?.Invoke(builderOptions);
+
+        RegisterServices(services);
+
+        return builder;
+    }
+
+    private static void ConfigureGlobalOptions(
+        Action<GlobalOptions>? configureGlobalOptions,
+        ConfigurationManager? configuration = null)
+    {
+        if (globalOptions.IsValueCreated)
+        {
+            LogGlobalOptionsWarning();
+        }
+        else
+        {
+            var globalOptionsValue = globalOptions.Value;
+            if (configuration != null)
+            {
+                var configSection = configuration.GetSection("NCronJob:GlobalOptions");
+
+                if (configSection.Exists())
+                {
+                    configSection.Bind(globalOptionsValue);
+                }
+            }
+
+            configureGlobalOptions?.Invoke(globalOptionsValue);
+        }
+    }
+
+    private static void RegisterServices(IServiceCollection services)
+    {
+        services.TryAddSingleton(globalOptions.Value.ToConcurrencySettings());
         services.AddHostedService<QueueWorker>();
         services.TryAddSingleton<JobRegistry>();
         services.TryAddSingleton<JobQueue>();
@@ -42,7 +120,25 @@ public static class NCronJobExtensions
         services.TryAddSingleton<IRetryHandler, RetryHandler>();
         services.TryAddSingleton<IInstantJobRegistry, InstantJobRegistry>();
         services.TryAddSingleton(TimeProvider.System);
-
-        return services;
     }
+
+    /// <summary>
+    /// Logs a warning about global options already being set.
+    /// We must create a logger factory because ILogger is not available
+    /// before building the WebApplication host.
+    /// </summary>
+    private static void LogGlobalOptionsWarning()
+    {
+        using var loggerFactory = LoggerFactory.Create(loggingBuilder => loggingBuilder
+            .SetMinimumLevel(LogLevel.Trace)
+            .AddConsole());
+
+        ILogger logger = loggerFactory.CreateLogger<GlobalOptions>();
+        GlobalOptionsWarning(logger, null);
+    }
+
+    private static readonly Action<ILogger, Exception?> GlobalOptionsWarning = LoggerMessage.Define(
+        LogLevel.Warning,
+        new EventId(0, "GlobalOptionsWarning"),
+        "Warning: GlobalOptions have already been set. Using existing values and ignoring new configuration.");
 }
