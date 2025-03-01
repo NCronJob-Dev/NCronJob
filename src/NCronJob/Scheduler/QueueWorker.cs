1f5f24ee57165e585c2f164f1e1d8e4f9ae9cae6
nulltoken <emeric.fermas@gmail.com>
2025-03-01 22:13:24 +0100

    Don't waste time scheduling disabled jobs

--- src/NCronJob/Scheduler/QueueWorker.cs
+++ src/NCronJob/Scheduler/QueueWorker.cs
@@ -176,7 +176,7 @@ internal sealed partial class QueueWorker : BackgroundService
 
     private void ScheduleInitialJobs()
     {
-        foreach (var job in jobRegistry.GetAllCronJobs())
+        foreach (var job in jobRegistry.GetAllCronJobs().Where(j => j.IsEnabled))
         {
             jobWorker.ScheduleJob(job);
         }
