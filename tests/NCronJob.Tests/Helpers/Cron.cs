34274d033b4700b200e568ec8120ab3c499578f2
nulltoken <emeric.fermas@gmail.com>
2025-03-09 19:00:00 +0100

    Reorganize tests

--- tests/NCronJob.Tests/Helpers/Cron.cs
+++ tests/NCronJob.Tests/Helpers/Cron.cs
@@ -11,5 +11,7 @@ public static class Cron
     public const string AtMinute2 = "2 * * * *";
     public const string AtMinute5 = "5 * * * *";
 
+    public const string AtEveryJanuaryTheFirst = "0 0 1 1 *";
+
     public const string Never = "* * 31 2 *";
 }
