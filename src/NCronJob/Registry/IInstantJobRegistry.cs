11a0d9bff7f804f575df47f6b4db9ac922a254e2
nulltoken <emeric.fermas@gmail.com>
2025-04-17 18:24:27 +0200

    Simplify IInstantJobRegistry core definition

--- src/NCronJob/Registry/IInstantJobRegistry.cs
+++ src/NCronJob/Registry/IInstantJobRegistry.cs
@@ -9,46 +9,6 @@ namespace NCronJob;
 public interface IInstantJobRegistry
 {
     /// <summary>
-    /// Queues an instant job to the JobQueue. The instance is retrieved from the container.
-    /// <param name="jobType">The job type. Expected to implement the <see cref="IJob"/> interface.</param>
-    /// <param name="parameter">An optional parameter that is passed down as the <see cref="JobExecutionContext"/> to the job.</param>
-    /// <param name="token">An optional token to cancel the job.</param>
-    /// </summary>
-    /// <returns>The job correlation id.</returns>
-    /// <remarks>
-    /// This is a fire-and-forget process, the Job will be queued with high priority and run in the background. The contents of <paramref name="parameter" />
-    /// are not serialized and deserialized. It is the reference to the <paramref name="parameter"/>-object that gets passed in.
-    /// </remarks>
-    /// <example>
-    /// Running a job with a parameter:
-    /// <code>
-    /// instantJobRegistry.RunInstantJob(typeof(MyJob), new MyParameterObject { Foo = "Bar" });
-    /// </code>
-    /// </example>
-    Guid RunInstantJob(Type jobType, object? parameter = null, CancellationToken token = default);
-
-    /// <summary>
-    /// Runs an instant job, which gets directly executed.
-    /// </summary>
-    /// <remarks>
-    /// The <paramref name="jobDelegate"/> delegate supports, like <see cref="NCronJobExtensions.AddNCronJob(IServiceCollection, Delegate, string, TimeZoneInfo)"/>, that services can be retrieved dynamically.
-    /// Also, the <see cref="CancellationToken"/> can be retrieved in this way.
-    /// </remarks>
-    /// <param name="jobDelegate">The delegate to execute.</param>
-    /// <param name="token">An optional token to cancel the job.</param>
-    /// <returns>The job correlation id.</returns>
-    Guid RunInstantJob(Delegate jobDelegate, CancellationToken token = default);
-
-    /// <summary>
-    /// Runs an instant job, which gets directly executed.
-    /// </summary>
-    /// <param name="jobName">The name of the job to execute.</param>
-    /// <param name="parameter">An optional parameter that is passed down as the <see cref="JobExecutionContext"/> to the job.</param>
-    /// <param name="token">An optional token to cancel the job.</param>
-    /// <returns>The job correlation id.</returns>
-    Guid RunInstantJob(string jobName, object? parameter = null, CancellationToken token = default);
-
-    /// <summary>
     /// Runs a job that will be executed after the given <paramref name="delay"/>.
     /// </summary>
     /// <param name="jobType">The job type. Expected to implement the <see cref="IJob"/> interface.</param>
@@ -165,56 +125,6 @@ public interface IInstantJobRegistry
     /// <param name="token">An optional token to cancel the job.</param>
     /// <returns>The job correlation id.</returns>
     Guid ForceRunScheduledJob(string jobName, TimeSpan delay, object? parameter = null, CancellationToken token = default);
-
-    /// <summary>
-    /// Runs an instant job to the registry, which will be executed even if the job is not registered and the concurrency is exceeded.
-    /// <param name="jobType">The job type. Expected to implement the <see cref="IJob"/> interface.</param>
-    /// <param name="parameter">An optional parameter that is passed down as the <see cref="JobExecutionContext"/> to the job.</param>
-    /// <param name="token">An optional token to cancel the job.</param>
-    /// </summary>
-    /// <returns>The job correlation id.</returns>
-    /// <remarks>
-    /// This is a fire-and-forget process, the Job will be run immediately in the background. The contents of <paramref name="parameter" />
-    /// are not serialized and deserialized. It is the reference to the <paramref name="parameter"/>-object that gets passed in.
-    /// </remarks>
-    /// <example>
-    /// Running a job with a parameter:
-    /// <code>
-    /// instantJobRegistry.ForceRunInstantJob(typeof(MyJob), new MyParameterObject { Foo = "Bar" });
-    /// </code>
-    /// </example>
-    Guid ForceRunInstantJob(Type jobType, object? parameter = null, CancellationToken token = default);
-
-    /// <summary>
-    /// Runs an instant job to the registry, which will be executed even if the job is not registered and the concurrency is exceeded.
-    /// <param name="jobName">The name of the job to execute.</param>
-    /// <param name="parameter">An optional parameter that is passed down as the <see cref="JobExecutionContext"/> to the job.</param>
-    /// <param name="token">An optional token to cancel the job.</param>
-    /// </summary>
-    /// <returns>The job correlation id.</returns>
-    /// <remarks>
-    /// This is a fire-and-forget process, the Job will be run immediately in the background. The contents of <paramref name="parameter" />
-    /// are not serialized and deserialized. It is the reference to the <paramref name="parameter"/>-object that gets passed in.
-    /// </remarks>
-    /// <example>
-    /// Running a job with a parameter:
-    /// <code>
-    /// instantJobRegistry.RunInstantJob("my_job", new MyParameterObject { Foo = "Bar" });
-    /// </code>
-    /// </example>
-    Guid ForceRunInstantJob(string jobName, object? parameter = null, CancellationToken token = default);
-
-    /// <summary>
-    /// Runs an instant job, which gets directly executed. The job will not be queued into the JobQueue, but executed directly.
-    /// </summary>
-    /// <remarks>
-    /// The <paramref name="jobDelegate"/> delegate supports, like <see cref="NCronJobExtensions.AddNCronJob(IServiceCollection, Delegate, string, TimeZoneInfo)"/>, that services can be retrieved dynamically.
-    /// Also, the <see cref="CancellationToken"/> can be retrieved in this way.
-    /// </remarks>
-    /// <param name="jobDelegate">The delegate to execute.</param>
-    /// <param name="token">An optional token to cancel the job.</param>
-    /// <returns>The job correlation id.</returns>
-    Guid ForceRunInstantJob(Delegate jobDelegate, CancellationToken token = default);
 }
 
 internal sealed partial class InstantJobRegistry : IInstantJobRegistry
@@ -241,17 +151,6 @@ internal sealed partial class InstantJobRegistry : IInstantJobRegistry
         this.observer = observer;
         this.logger = logger;
     }
-
-    /// <inheritdoc />
-    public Guid RunInstantJob(Type jobType, object? parameter = null, CancellationToken token = default)
-        => RunScheduledJob(jobType, TimeSpan.Zero, parameter, token);
-
-    /// <inheritdoc />
-    public Guid RunInstantJob(string jobName, object? parameter = null, CancellationToken token = default)
-        => RunScheduledJob(jobName, TimeSpan.Zero, parameter, token);
-
-    /// <inheritdoc />
-    public Guid RunInstantJob(Delegate jobDelegate, CancellationToken token = default) => RunScheduledJob(jobDelegate, TimeSpan.Zero, token);
 
     /// <inheritdoc />
     public Guid RunScheduledJob(Type jobType, TimeSpan delay, object? parameter = null, CancellationToken token = default)
@@ -311,18 +210,6 @@ internal sealed partial class InstantJobRegistry : IInstantJobRegistry
     /// <inheritdoc />
     public Guid ForceRunScheduledJob(Delegate jobDelegate, DateTimeOffset startDate, CancellationToken token = default) =>
         RunDelegateJob(jobDelegate, startDate, true, token);
-
-    /// <inheritdoc />
-    public Guid ForceRunInstantJob(Delegate jobDelegate, CancellationToken token = default) =>
-        ForceRunScheduledJob(jobDelegate, TimeSpan.Zero, token);
-
-    /// <inheritdoc />
-    public Guid ForceRunInstantJob(Type jobType, object? parameter = null, CancellationToken token = default)
-        => ForceRunScheduledJob(jobType, TimeSpan.Zero, parameter, token);
-
-    /// <inheritdoc />
-    public Guid ForceRunInstantJob(string jobName, object? parameter = null, CancellationToken token = default)
-        => ForceRunScheduledJob(jobName, TimeSpan.Zero, parameter, token);
 
     private Guid RunDelegateJob(Delegate jobDelegate, DateTimeOffset startDate, bool forceExecution = false, CancellationToken token = default)
     {
--- src/NCronJob/Registry/IInstantJobRegistryExtensions.cs
+++ src/NCronJob/Registry/IInstantJobRegistryExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace NCronJob;
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace NCronJob;
 
 /// <summary>
 /// Extensions for IInstantJobRegistry.
@@ -31,6 +33,75 @@ public static class IInstantJobRegistryExtensions
         ArgumentNullException.ThrowIfNull(instantJobRegistry);
 
         return instantJobRegistry.RunInstantJob(typeof(TJob), parameter, token);
+    }
+
+    /// <summary>
+    /// Queues an instant job to the JobQueue. The instance is retrieved from the container.
+    /// <param name="instantJobRegistry">The instant job registry.</param>
+    /// <param name="jobType">The job type. Expected to implement the <see cref="IJob"/> interface.</param>
+    /// <param name="parameter">An optional parameter that is passed down as the <see cref="JobExecutionContext"/> to the job.</param>
+    /// <param name="token">An optional token to cancel the job.</param>
+    /// </summary>
+    /// <returns>The job correlation id.</returns>
+    /// <remarks>
+    /// This is a fire-and-forget process, the Job will be queued with high priority and run in the background. The contents of <paramref name="parameter" />
+    /// are not serialized and deserialized. It is the reference to the <paramref name="parameter"/>-object that gets passed in.
+    /// </remarks>
+    /// <example>
+    /// Running a job with a parameter:
+    /// <code>
+    /// instantJobRegistry.RunInstantJob(typeof(MyJob), new MyParameterObject { Foo = "Bar" });
+    /// </code>
+    /// </example>
+    public static Guid RunInstantJob(
+        this IInstantJobRegistry instantJobRegistry,
+        Type jobType,
+        object? parameter = null,
+        CancellationToken token = default)
+    {
+        ArgumentNullException.ThrowIfNull(instantJobRegistry);
+
+        return instantJobRegistry.RunScheduledJob(jobType, TimeSpan.Zero, parameter, token);
+    }
+
+    /// <summary>
+    /// Runs an instant job, which gets directly executed.
+    /// </summary>
+    /// <remarks>
+    /// The <paramref name="jobDelegate"/> delegate supports, like <see cref="NCronJobExtensions.AddNCronJob(IServiceCollection, Delegate, string, TimeZoneInfo)"/>, that services can be retrieved dynamically.
+    /// Also, the <see cref="CancellationToken"/> can be retrieved in this way.
+    /// </remarks>
+    /// <param name="instantJobRegistry">The instant job registry.</param>
+    /// <param name="jobDelegate">The delegate to execute.</param>
+    /// <param name="token">An optional token to cancel the job.</param>
+    /// <returns>The job correlation id.</returns>
+    public static Guid RunInstantJob(
+        this IInstantJobRegistry instantJobRegistry,
+        Delegate jobDelegate,
+        CancellationToken token = default)
+    {
+        ArgumentNullException.ThrowIfNull(instantJobRegistry);
+
+        return instantJobRegistry.RunScheduledJob(jobDelegate, TimeSpan.Zero, token);
+    }
+
+    /// <summary>
+    /// Runs an instant job, which gets directly executed.
+    /// </summary>
+    /// <param name="instantJobRegistry">The instant job registry.</param>
+    /// <param name="jobName">The name of the job to execute.</param>
+    /// <param name="parameter">An optional parameter that is passed down as the <see cref="JobExecutionContext"/> to the job.</param>
+    /// <param name="token">An optional token to cancel the job.</param>
+    /// <returns>The job correlation id.</returns>
+    public static Guid RunInstantJob(
+        this IInstantJobRegistry instantJobRegistry,
+        string jobName,
+        object? parameter = null,
+        CancellationToken token = default)
+    {
+        ArgumentNullException.ThrowIfNull(instantJobRegistry);
+
+        return instantJobRegistry.RunScheduledJob(jobName, TimeSpan.Zero, parameter, token);
     }
 
     /// <summary>
@@ -100,5 +171,84 @@ public static class IInstantJobRegistryExtensions
         ArgumentNullException.ThrowIfNull(instantJobRegistry);
 
         return instantJobRegistry.ForceRunInstantJob(typeof(TJob), parameter, token);
+    }
+
+    /// <summary>
+    /// Runs an instant job to the registry, which will be executed even if the job is not registered and the concurrency is exceeded.
+    /// <param name="instantJobRegistry">The instant job registry.</param>
+    /// <param name="jobType">The job type. Expected to implement the <see cref="IJob"/> interface.</param>
+    /// <param name="parameter">An optional parameter that is passed down as the <see cref="JobExecutionContext"/> to the job.</param>
+    /// <param name="token">An optional token to cancel the job.</param>
+    /// </summary>
+    /// <returns>The job correlation id.</returns>
+    /// <remarks>
+    /// This is a fire-and-forget process, the Job will be run immediately in the background. The contents of <paramref name="parameter" />
+    /// are not serialized and deserialized. It is the reference to the <paramref name="parameter"/>-object that gets passed in.
+    /// </remarks>
+    /// <example>
+    /// Running a job with a parameter:
+    /// <code>
+    /// instantJobRegistry.ForceRunInstantJob(typeof(MyJob), new MyParameterObject { Foo = "Bar" });
+    /// </code>
+    /// </example>
+    public static Guid ForceRunInstantJob(
+        this IInstantJobRegistry instantJobRegistry,
+        Type jobType,
+        object? parameter = null,
+        CancellationToken token = default)
+    {
+        ArgumentNullException.ThrowIfNull(instantJobRegistry);
+
+        return instantJobRegistry.ForceRunScheduledJob(jobType, TimeSpan.Zero, parameter, token);
+    }
+
+    /// <summary>
+    /// Runs an instant job to the registry, which will be executed even if the job is not registered and the concurrency is exceeded.
+    /// <param name="instantJobRegistry">The instant job registry.</param>
+    /// <param name="jobName">The name of the job to execute.</param>
+    /// <param name="parameter">An optional parameter that is passed down as the <see cref="JobExecutionContext"/> to the job.</param>
+    /// <param name="token">An optional token to cancel the job.</param>
+    /// </summary>
+    /// <returns>The job correlation id.</returns>
+    /// <remarks>
+    /// This is a fire-and-forget process, the Job will be run immediately in the background. The contents of <paramref name="parameter" />
+    /// are not serialized and deserialized. It is the reference to the <paramref name="parameter"/>-object that gets passed in.
+    /// </remarks>
+    /// <example>
+    /// Running a job with a parameter:
+    /// <code>
+    /// instantJobRegistry.RunInstantJob("my_job", new MyParameterObject { Foo = "Bar" });
+    /// </code>
+    /// </example>
+    public static Guid ForceRunInstantJob(
+        this IInstantJobRegistry instantJobRegistry,
+        string jobName,
+        object? parameter = null,
+        CancellationToken token = default)
+    {
+        ArgumentNullException.ThrowIfNull(instantJobRegistry);
+
+        return instantJobRegistry.ForceRunScheduledJob(jobName, TimeSpan.Zero, parameter, token);
+    }
+
+    /// <summary>
+    /// Runs an instant job, which gets directly executed. The job will not be queued into the JobQueue, but executed directly.
+    /// <param name="instantJobRegistry">The instant job registry.</param>
+    /// </summary>
+    /// <remarks>
+    /// The <paramref name="jobDelegate"/> delegate supports, like <see cref="NCronJobExtensions.AddNCronJob(IServiceCollection, Delegate, string, TimeZoneInfo)"/>, that services can be retrieved dynamically.
+    /// Also, the <see cref="CancellationToken"/> can be retrieved in this way.
+    /// </remarks>
+    /// <param name="jobDelegate">The delegate to execute.</param>
+    /// <param name="token">An optional token to cancel the job.</param>
+    /// <returns>The job correlation id.</returns>
+    public static Guid ForceRunInstantJob(
+        this IInstantJobRegistry instantJobRegistry,
+        Delegate jobDelegate,
+        CancellationToken token = default)
+    {
+        ArgumentNullException.ThrowIfNull(instantJobRegistry);
+
+        return instantJobRegistry.ForceRunScheduledJob(jobDelegate, TimeSpan.Zero, token);
     }
 }
