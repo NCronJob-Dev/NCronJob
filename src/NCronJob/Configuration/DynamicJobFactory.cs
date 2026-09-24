b158647b17d43b87c14e630571ad11f55dc60414
Steven Giesel <stgiesel35@gmail.com>
2026-09-24 20:35:05 +0200

    Compile dynamic job invoker once per delegate

--- src/NCronJob/Configuration/DynamicJobFactory.cs
+++ src/NCronJob/Configuration/DynamicJobFactory.cs
@@ -1,51 +1,67 @@
 using System.Reflection;
+using System.Runtime.CompilerServices;
 
 namespace NCronJob;
 
 internal sealed class DynamicJobFactory : IJob
 {
+    // Compiling the invoker is expensive and the delegate is fixed per job definition, so compile once per delegate.
+    private static readonly ConditionalWeakTable<Delegate, CompiledJob> CompiledJobs = new();
+
     private readonly IServiceProvider serviceProvider;
-    private readonly Func<object[], Task> invoker;
-    private readonly Func<IServiceProvider, object>?[] serviceResolvers;
-    private readonly ParameterInfo[] parameters;
+    private readonly CompiledJob compiledJob;
 
     public DynamicJobFactory(IServiceProvider serviceProvider, Delegate jobAction)
     {
         ArgumentNullException.ThrowIfNull(jobAction);
 
         this.serviceProvider = serviceProvider;
-        parameters = jobAction.Method.GetParameters();
-        serviceResolvers = ServiceResolverHelper.BuildServiceResolvers(parameters);
-        invoker = BuildInvoker(jobAction);
-    }
-
-    private static Func<object[], Task> BuildInvoker(Delegate jobDelegate)
-    {
-        var returnType = jobDelegate.Method.ReturnType;
-
-        if (returnType == typeof(Task))
-        {
-            return DelegateInvoker.Build<Task>(jobDelegate);
-        }
-
-        if (returnType == typeof(void))
-        {
-            var action = DelegateInvoker.BuildAction(jobDelegate);
-            return objects => { action(objects); return Task.CompletedTask; };
-        }
-
-        throw new InvalidOperationException("The job action must return a Task or void type.");
+        compiledJob = CompiledJobs.GetValue(jobAction, static action => new CompiledJob(action));
     }
 
     public Task RunAsync(IJobExecutionContext context, CancellationToken token)
     {
         var arguments = ServiceResolverHelper.ResolveArguments(
             serviceProvider,
-            parameters,
-            serviceResolvers,
+            compiledJob.Parameters,
+            compiledJob.ServiceResolvers,
             context,
             token);
 
-        return invoker(arguments);
+        return compiledJob.Invoker(arguments);
+    }
+
+    private sealed class CompiledJob
+    {
+        public CompiledJob(Delegate jobAction)
+        {
+            Parameters = jobAction.Method.GetParameters();
+            ServiceResolvers = ServiceResolverHelper.BuildServiceResolvers(Parameters);
+            Invoker = BuildInvoker(jobAction);
+        }
+
+        public ParameterInfo[] Parameters { get; }
+
+        public Func<IServiceProvider, object>?[] ServiceResolvers { get; }
+
+        public Func<object[], Task> Invoker { get; }
+
+        private static Func<object[], Task> BuildInvoker(Delegate jobDelegate)
+        {
+            var returnType = jobDelegate.Method.ReturnType;
+
+            if (returnType == typeof(Task))
+            {
+                return DelegateInvoker.Build<Task>(jobDelegate);
+            }
+
+            if (returnType == typeof(void))
+            {
+                var action = DelegateInvoker.BuildAction(jobDelegate);
+                return objects => { action(objects); return Task.CompletedTask; };
+            }
+
+            throw new InvalidOperationException("The job action must return a Task or void type.");
+        }
     }
 }
